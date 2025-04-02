a46f8dec8437f7bac240be559e4771a5c68e518c
anup kushwaha <anupk6347@gmail.com>
2025-04-03 00:50:00 +0530

    second commit_vite

--- clients/src/main.jsx
+++ clients/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import RoutesMain from "./routes/routesmain";
 import AuthProvider from "./context/auth-context.jsx";
