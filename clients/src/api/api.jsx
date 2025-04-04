14c714aba0c774158447af817d4bf9dbdf1c1a12
Anup Kushwaha <125217613+anupkushwaha8081@users.noreply.github.com>
2025-04-04 13:56:56 +0530

    Update api.jsx

--- clients/src/api/api.jsx
+++ clients/src/api/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:3096",
-  baseURL: "https://karna-s-gurukul-vjav.vercel.app/",
+  baseURL: "https://karanabackendrepo-production.up.railway.app/",
 
 });
 
